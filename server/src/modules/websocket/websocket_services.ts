8e0401f38a5f7e6fea6f4948286f5052e09d6e93
kaiquye <kaiquemendesilva82@gmail.com>
2022-05-28 18:42:42 -0300

    websocket : ajustando cometarios

--- server/src/modules/websocket/websocket_services.ts
+++ server/src/modules/websocket/websocket_services.ts
@@ -2,6 +2,13 @@ import { Socket } from "socket.io";
 import Imessage from "./websocket_interface";
 import WebsocketData from "./websocket_data";
 
+/**
+ * @variation io : instacia do servidor web socket.
+ * @method WebsocketData : Gerencia os dados me memoria.
+ * @method Room : inicia uma nova sala.
+ * @method Message : gerencia todas as mensagens.
+ */
+
 class WebSocketServices {
   io: Socket;
 
@@ -14,7 +21,9 @@ class WebSocketServices {
   private Room(): void {
     this.io.on("nova_sala", (data: Imessage) => {
       this.io.join(data.sala);
-      this.io.to(data.sala).emit("sala_mensagens", WebsocketData.findByRoom(data));
+      this.io
+        .to(data.sala)
+        .emit("sala_mensagens", WebsocketData.findByRoom(data));
     });
   }
 
@@ -22,7 +31,9 @@ class WebSocketServices {
     this.io.on("nova_mensagem", (data: Imessage) => {
       console.log(data);
       WebsocketData.newMessage(data);
-      this.io.to(data.sala).emit('sala_mensagens',  WebsocketData.findByRoom(data))
+      this.io
+        .to(data.sala)
+        .emit("sala_mensagens", WebsocketData.findByRoom(data));
     });
   }
 }
