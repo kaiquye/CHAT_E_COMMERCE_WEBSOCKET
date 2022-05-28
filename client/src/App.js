68e6e87240cbf31d763d3a433329b145aec8edde
kaiquye <kaiquemendesilva82@gmail.com>
2022-05-28 15:45:22 -0300

    websocket : configurando servidor

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client';
+import { useEffect } from 'react';
 
 function App() {
+
+  const Socket = io("ws://localhost:4000", {
+    transports: ["websocket"],
+  })
+  Socket.emit('teste', 'teste')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teste</h1>
     </div>
   );
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import helmetConfig from "./middlewares/helmet/index";
 import corsConfig from "./middlewares/cors/index";
+import socket, { Socket } from "socket.io";
+import http from "http";
 
 class Server {
+  public server;
   public App;
+  public io;
 
   constructor() {
     this.App = express();
+    this.server = http.createServer(this.App);
+    this.io = new socket.Server(this.server);
     this.Middleware();
     this.Routes();
   }
@@ -18,10 +24,12 @@ class Server {
   }
 
   private Routes(): void {
+    this.io.on("connection", (emit) => this.teste(emit));
     this.App.get("/teste", (req, res) => {
       console.log(req);
     });
   }
+  
 }
 
-export default new Server().App;
+export default new Server().server;
