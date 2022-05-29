a38bb9d64158ae108f4af6f9ca423a1c3fe8f8e5
kaiquye <kaiquemendesilva82@gmail.com>
2022-05-28 22:38:11 -0300

    authentication JWT : configurando autenticação

---
+++ server/src/middlewares/auth/index.ts
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import JWT, { Jwt, Secret } from "jsonwebtoken";
+import express, { NextFunction, Request, Response } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+import { AppError } from "../../util/AppError";
+
+interface IAuth {
+  authManager(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): NextFunction | Response | void;
+  newTokenManager(email: string): string;
+}
+
+class Auth implements IAuth {
+  authManager(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): NextFunction | Response | void {
+    try {
+      const token = req.headers["authorization"];
+      if (!token) {
+        return res
+          .status(401)
+          .json(new AppError(400, "token não foi informado."));
+      }
+      let Secret = process.env.SECRET || "";
+      const check = JWT.verify(token, Secret);
+      if (check) {
+        return next();
+      } else {
+        return res.status(401).json(new AppError(401, "token invalido"));
+      }
+    } catch (error) {
+      return res.status(401).json(new AppError(401, "token invalido"));
+    }
+  }
+
+  newTokenManager(email: string): string {
+    const secret: string = process.env.SECRET || "";
+    const hash = JWT.sign(email, secret, { expiresIn: "1800s" });
+    return hash;
+  }
+}
+
+export default new Auth();
