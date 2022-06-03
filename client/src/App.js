ae46cb28b323c93b89a2e694783de539382bb8c2
KAIC MENDES <69175890+kaiquye@users.noreply.github.com>
2022-06-03 09:36:38 -0300

    client : apagando documentação

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,10 @@ import { useEffect } from 'react';
 
 function App() {
 
-  // const Socket = io("ws://localhost:4000", {
-  //   transports: ["websocket"],
-  // })
-
-
-  // Socket.emit('nova_sala', { sala: 'kaique', mensagem: 'ola' })
-
-  // Socket.on('sala_mensagens', (data) => {
-  //   console.log(data)
-  // })
-
-  // Socket.emit('lista_mensagens', { sala: 'kaique', mensagem: 'ola' })
-
-  // Socket.on('lista_mensagens', (data) => {
-  //   console.log(data)
-  // })
 
   return (
     <BrowserRouter>
       <div className="App">
-        {/* <button onClick={() => {
-          Socket.emit('nova_mensagem', { sala: 'kaique', mensagem: 'ola' });
-        }} >
-          teste
-        </button> */}
         <RoutesApp />
       </div>
     </BrowserRouter>
